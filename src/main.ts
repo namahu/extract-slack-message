e768a949df90c306271d5927f6da419d6ac35227
namahu <neti0326@gmail.com>
2022-10-13 13:18:41 +0900

    feat: 特定チャンネルのメッセージ取得処理を実実装中

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-const extractSlackMessage = () => {};
+type Properties = {
+    SLACK_TOKEN: string;
+    SLACK_API_BASE_URL: string; 
+    TARGET_CHANNEL_ID: string;
+    OLDEST: string;
+}
+
+const extractSlackMessage = () => {
+
+    const scriptProperties = PropertiesService.getScriptProperties();
+    const properties = scriptProperties.getProperties() as unknown as Properties;
+
+    const requestOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+        method: 'get',
+        headers: {
+            Authorization: `Bearer ${properties.SLACK_TOKEN}`,
+            "Content-Type": "application/json"
+        },
+        muteHttpExceptions: true
+    };
+
+    const url = properties.SLACK_API_BASE_URL
+        + "conversations.history?"
+        + "channel=" + properties.TARGET_CHANNEL_ID;
+
+    const result = UrlFetchApp.fetch(
+        url,
+        requestOptions
+    );
+
+    Logger.log(result);
+
+};
